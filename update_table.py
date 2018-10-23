6436301ccd1738aa45d15c77b03d5e60e80160d5
Georgios Giasemidis <g.giasemidis@gmail.com>
2018-10-23 01:16:36 +0100

    update table through merging of two tables

--- update_table.py
+++ update_table.py
@@ -15,57 +15,59 @@ win_unicode_console.enable()
 
 def main(table_file, scores_file, out_file):
     '''To DO:
-        1) improve performance and design, use dataframe merge
-        2) Insert simple models for benchmarking
+        1) Insert simple models for benchmarking
     '''
 
     table = pd.read_csv(table_file)
-    scores = read_json(scores_file)
-
+#    scores = read_json(scores_file)
+    scores = pd.read_csv(scores_file)
+    
     # check if there is a new user, give the lowest score.
-    min_score = np.min(table.Points.values)
-    new_users = [[0, u, 0, min_score, 0] for u in scores.keys() if u not in 
-                 table.Name.values]
-    if new_users:
-        print('Warning: There are new players in the game.')
-        for r in new_users:
-            print(r[1])
-        df_new = pd.DataFrame(data=new_users, columns=list(table.keys()))
+    min_points = np.min(table.Points.values)
     
-        table = table.append(df_new, ignore_index=True)
-#    print(table)
-#    return
+    # find mvp score of the round
+    mvp_score = np.nanmax(scores['Score'].values)
+    
+    # find min score of the round
+    min_score = np.nanmin(scores['Score'].values)
+    
+    # merge datasets
+    df_merged = table.merge(scores, how='outer', 
+                            left_on='Name', right_on='Name')
+    df_new = df_merged.copy()
 
-    points = table.Points.values
-    mvps = table.MVP.values
-    misses = table['Missed Rounds'].values
+    # new players
+    jj = np.isnan(df_merged['Points'])
+    if any(jj):
+        df_new.loc[jj, 'Points'] = min_points
+        df_new.loc[jj, 'MVP'] = 0
+        df_new.loc[jj, 'Missed Rounds'] = 0        
+        for name in df_new['Name'][jj].values:
+            print('%s is a new player' % name)
 
-    # find mvp score
-    score_list = [scores[u] for u in scores.keys()]
-    mvp_score = max(score_list)
-    min_score = min(score_list)
-    
-    # update table
-    for i, name in enumerate(table.Name.values):
-        if name in scores.keys():
-            score = scores[name]
-            points[i] += score
-            if score == mvp_score:
-                mvps[i] += 1
-        else:
-            points[i] += min_score
-            misses[i] += 1
-    
+    # did not play this round
+    ii = np.isnan(df_merged['Score'])
+    if any(ii):
+        df_new.loc[ii, 'Score'] = min_score
+        df_new.loc[ii, 'Missed Rounds'] += 1
+        for name in df_new['Name'][ii].values:
+            print('Warning: %s did not play this round' % name)
+
     # check if user has missed more than four rounds.
-    mm = np.where(misses >= 4)[0]
-    if mm:
-        for m in mm:
-            print('Warning: User %s has missed four rounds' % m)
-
-    new_table = table[['Name', 'MVP', 'Points', 'Missed Rounds']].copy()
-    new_table['MVP'] = mvps
-    new_table['Points'] = points
-    new_table['Missed Rounds'] = misses
+    mm = df_new['Missed Rounds'] >= 4
+    if any(mm):
+        for name in df_new['Name'][mm].values:
+            print('Warning: User %s has missed four rounds' % name)
+        
+    # update points
+    df_new['Points'] += df_new['Score'].astype(int)
+    # update MVP
+    df_new.loc[df_new['Score']==mvp_score, 'MVP'] += 1
+    # convert colums to int type.
+    df_new[['MVP', 'Points', 'Missed Rounds']] = df_new[['MVP', 'Points', 'Missed Rounds']].astype(int)
+    
+    new_table = df_new[['Name', 'MVP', 'Points', 'Missed Rounds']].copy()
+    # sort by points (desc), then by MVP (desc) and finally by name (asc)
     new_table = new_table.sort_values(by=['Points', 'MVP', 'Name'], 
                                       ascending=[False, False, True])
     new_table.insert(0, 'Position', 
