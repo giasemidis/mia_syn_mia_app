c25695b6bc11a3c7b38d085d3a4f30b91594277e
Georgios Giasemidis <g.giasemidis@gmail.com>
2019-10-20 12:30:01 +0100

    argparse design

--- extract_playoff_predictions.py
+++ extract_playoff_predictions.py
@@ -1,39 +1,61 @@
-from auxiliary.io_json import read_json
+import argparse
 import pandas as pd
 import regex
+from auxiliary.io_json import read_json
 
-comments = read_json('testing/temp3.json')
 
-teams = ['Alba Berlin', 'Anadolu Efes', 'Armani Milano', 'Crvena Zvezda',
-         'CSKA Moscow', 'FC Barcelona', 'FC Bayern Munich',
-         'Fenerbahce Istanbul', 'Khimki Moscow region',
-         'Baskonia Vitoria-Gasteiz', 'ASVEL Villeurbanne',
-         'Maccabi Tel Aviv', 'Olympiakos Piraeus', 'Panathinaikos Athens',
-         'Real Madrid', 'Valencia Basket', 'Zalgiris Kaunas',
-         'Zenit St. Petersburg']
-regexps = ['(?e)(%s){e<=2}' % team for team in teams]
-n_pred_teams = 8
-header = ['team%d' % i for i in range(1, n_pred_teams + 1)]
+def main(comments):
+    '''
+    Function that extracts the username and the predicted teams of the playoffs
+    from the comments (of a facebok post) and converts them to a dataframe,
+    which is saved to the disk.
+    '''
+    teams = ['Alba Berlin', 'Anadolu Efes', 'Armani Milano', 'Crvena Zvezda',
+             'CSKA Moscow', 'FC Barcelona', 'FC Bayern Munich',
+             'Fenerbahce Istanbul', 'Khimki Moscow region',
+             'Baskonia Vitoria-Gasteiz', 'ASVEL Villeurbanne',
+             'Maccabi Tel Aviv', 'Olympiakos Piraeus', 'Panathinaikos Athens',
+             'Real Madrid', 'Valencia Basket', 'Zalgiris Kaunas',
+             'Zenit St. Petersburg']
+    regexps = ['(?e)(%s){e<=2}' % team for team in teams]
+    n_pred_teams = 8
+    header = ['team%d' % i for i in range(1, n_pred_teams + 1)]
 
-all_predictions = []
-usernames = []
-for comment in comments:
-    text = comment['message']
-    text_copy = text.replace('\n', '')
-    pred_teams = []
-    for team, rxp in zip(teams, regexps):
-        r = regex.search(rxp, text)
-        if r is not None:
-            match = text[r.start():r.end()]
-            pred_teams.append(team)
-            text_copy = text_copy.replace(match, '').strip()
-    username = text_copy.replace('-', '').strip()
-    if len(pred_teams) == n_pred_teams:
-        all_predictions.append(dict(zip(header, sorted(pred_teams))))
-        usernames.append(username)
-    else:
-        print(text+'\n')
+    all_predictions = []
+    usernames = []
+    for comment in comments:
+        text = comment['message']
+        text_copy = text.replace('\n', '')
+        pred_teams = []
+        for team, rxp in zip(teams, regexps):
+            r = regex.search(rxp, text)
+            if r is not None:
+                match = text[r.start():r.end()]
+                pred_teams.append(team)
+                text_copy = text_copy.replace(match, '').strip()
+        username = text_copy.replace('-', '').strip()
+        if len(pred_teams) == n_pred_teams:
+            all_predictions.append(dict(zip(header, sorted(pred_teams))))
+            usernames.append(username)
+        else:
+            print(text+'\n')
 
-df = pd.DataFrame(all_predictions, index=usernames)
-df.index.name = 'username'
-df.to_csv('output/playoffs_predictions.csv', index=True)
+    # convert to dataframe
+    df = pd.DataFrame(all_predictions, index=usernames)
+    df.index.name = 'username'
+    # save to file
+    df.to_csv('output/playoffs_predictions.csv', index=True)
+    return
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--file', type=str,
+                        required=argparse.FileType('r'),
+                        help="the file with facebook comments")
+    args = parser.parse_args()
+
+    # read comments
+    comments = read_json(args.file)
+
+    main(comments)
--- fetch_playoff_predictions.py
+++ fetch_playoff_predictions.py
@@ -1,3 +1,4 @@
+import argparse
 # import numpy as np
 # import pandas as pd
 from datetime import datetime
@@ -9,40 +10,51 @@ import facebook as fb
 import requests
 from auxiliary.io_json import read_json, write_json
 
-post_id = '2723829427651239'
-tokens_file = 'config/tokens.json'
-tokens = read_json(tokens_file)
-token = tokens['token']
-user_id = tokens['user_id']
 
-fb_format = '%Y-%m-%dT%H:%M:%S+0000'
+def main(post_id):
+    tokens_file = 'config/tokens.json'
+    tokens = read_json(tokens_file)
+    token = tokens['token']
+    user_id = tokens['user_id']
 
-# make graph
-graph = fb.GraphAPI(access_token=token, version=3.0)
-# graph id = user_id_post_id
-idd = user_id + '_' + post_id
-# get text of post
-post = graph.get_object(id=idd)
-message = post['message']
-post_time = datetime.strptime(post['created_time'], fb_format)
+    fb_format = '%Y-%m-%dT%H:%M:%S+0000'
 
-# Get the comments from a post.
-comments = graph.get_connections(id=idd, connection_name='comments')
-answers = []
-answers.extend(comments['data'])
-# %%
-while True:
-    for comment in comments['data']:
-        comment_id = comment['id']
-        text = comment['message']
-        time = datetime.strptime(comment['created_time'], fb_format)
-        # make the time variable datetime aware
-        time = time.replace(tzinfo=pytz.UTC)
+    # make graph
+    graph = fb.GraphAPI(access_token=token, version=3.0)
+    # graph id = user_id_post_id
+    idd = user_id + '_' + post_id
+    # get text of post
+    # post = graph.get_object(id=idd)
+    # message = post['message']
+    # post_time = datetime.strptime(post['created_time'], fb_format)
 
-    if 'next' in comments['paging']:
-        comments = requests.get(comments['paging']['next']).json()
-        answers.extend(comments['data'])
-    else:
-        break
+    # Get the comments from a post.
+    comments = graph.get_connections(id=idd, connection_name='comments')
+    answers = []
+    answers.extend(comments['data'])
+    # %%
+    while True:
+        for comment in comments['data']:
+            # comment_id = comment['id']
+            # text = comment['message']
+            time = datetime.strptime(comment['created_time'], fb_format)
+            # make the time variable datetime aware
+            time = time.replace(tzinfo=pytz.UTC)
 
-write_json('tem3.json', answers)
+        if 'next' in comments['paging']:
+            comments = requests.get(comments['paging']['next']).json()
+            answers.extend(comments['data'])
+        else:
+            break
+
+    write_json('output/playoff_predictions_fb_comments.json', answers)
+    return
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--post_id', type=str, required=True,
+                        help="the id of the post")
+    args = parser.parse_args()
+
+    main(args.post_id)
